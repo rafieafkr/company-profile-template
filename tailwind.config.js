1b18f9686b616f2c896b8372475f1ecdf839d419
unknown <rafie.afkar@gmail.com>
2022-10-02 01:52:32 +0700

    darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
